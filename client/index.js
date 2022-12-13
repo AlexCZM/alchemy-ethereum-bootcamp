1127366ecf21011e43e0afeaac8f1444d71ea157
AlexCZM <alexczm@proton.me>
2022-12-13 21:47:22 +0200

    Init repo week 2

--- client/index.js
+++ client/index.js
@@ -1,14 +1,25 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  /* in order to check if a name (data) is in the Merkle tree we need:
+   *   - name -> or leaf to be checked if it's in the tree
+   *   - proof -> merkle tree path to create root from name
+   *   - root -> it's assumed the server keeps track of it
+   * */
+
+  const name = "Shelby Toy"; // change the name
+  const merkleTree = new MerkleTree(niceList);
+
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
